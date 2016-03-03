17c189f315a69f8f4d098673d7f50ce813968b10
Gabriel Isenberg <gabe@attackpattern.com>
2016-03-02 16:33:19 -0800

    Rename index.js => Configuration.js

---
+++ src/config/Configuration.js
@@ -0,0 +1,36 @@
+import { remote } from 'electron';
+import path from 'path';
+import fs from 'fs';
+
+export default class Configuration {
+  constructor() {
+    const configDir = path.join(remote.app.getPath('appData'), 'Codiff');
+    if(!fs.existsSync(configDir)) {
+      fs.mkdirSync(configDir);
+    }
+
+    this.configPath = path.join(configDir, 'config.json');
+    if(!fs.existsSync(this.configPath)) {
+      fs.writeFileSync(this.configPath, JSON.stringify({}, null, 2));
+    }
+
+    this.values = require(this.configPath);
+  }
+
+  get projects() {
+    return this.get('projects') || [];
+  }
+
+  get(key) {
+    return this.values[key];
+  }
+
+  set(key, value) {
+    this.values[key] = value;
+    this.commit();
+  }
+
+  commit() {
+    fs.writeFileSync(this.configPath, JSON.stringify(this.values, null, 2));
+  }
+}
